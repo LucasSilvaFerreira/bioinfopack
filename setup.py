c16a32c94b2385b1d3a2f2615fe692e8dd2092f0
lucas <bioinfolucas@gmail.com>
2016-02-02 18:08:12 -0200

    creating setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='bioinfopack',
+        version='',
+        packages=['', 'tools'],
+        url='',
+        license='',
+        author='Lucas Ferreira da Silva',
+        author_email='lucasfsilva@usp.br',
+        description=''
+)
